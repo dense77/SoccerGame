4d87d4e3ad79d8b801d2c8b86618048b87cd8f23
16417 <1641782317@qq.com>
2026-06-13 16:55:46 +0800

    fix: ignore .vs in vite watch

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'node',
-    include: ['src/test/**/*.test.ts'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
+  server: {
+    watch: {
+      ignored: ['**/.vs/**'],
     },
   },
 })
---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'node',
+      include: ['src/test/**/*.test.ts'],
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html'],
+      },
+    },
+  }),
+)
